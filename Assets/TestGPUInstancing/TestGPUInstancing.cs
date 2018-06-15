c4056bd9b686a9d967e9f39f1fc1f091477f5279
Flag <karexy@qq.com>
2018-06-15 19:06:19 +0800

    update

---
+++ Assets/TestGPUInstancing/TestGPUInstancing.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class TestGPUInstancing : MonoBehaviour
+{
+    private void Start()
+    {
+        var x = Random.Range(-15f, 15f);
+        var y = Random.Range(-15f, 15f);
+        var z = Random.Range(-15f, 15f);
+        transform.position = new Vector3(x, y, z);
+    }
+}
