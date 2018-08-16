38bf717fc38bf58aa93e633c9f1d4592b1643bbb
Flag <karexy@qq.com>
2018-08-16 18:31:25 +0800

    update

---
+++ Assets/TestPostProcessing/TestPostProcessing.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+public class TestPostProcessing : MonoBehaviour
+{
+    private void Start()
+    {
+        var shader = Shader.Find("Hidden/TestPostProcessing");
+        _processingMat = new Material(shader);
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        
+        Graphics.Blit(source, destination, _processingMat);
+    }
+
+    private Material _processingMat;
+}
---
+++ Assets/TestTimeScale.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTimeScale : MonoBehaviour
+{
+    private void Start()
+    {
+        Time.timeScale = 0.01f;
+    }
+
+    private void Update()
+    {
+//         Debug.Log(Time.deltaTime);
+//         Debug.Log("Update");
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.Log(Time.fixedDeltaTime);
+        Debug.Log("FixedUpdate");
+    }
+}
