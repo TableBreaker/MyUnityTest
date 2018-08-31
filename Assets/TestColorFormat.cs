6094f56ba317ff5def00ab55f7fe2de5affb5b80
Flag <karexy@qq.com>
2018-08-31 16:36:44 +0800

    update

---
+++ Assets/TestColorFormat.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestColorFormat : MonoBehaviour
+{
+    private void Start()
+    {
+        var color = new Color(0.1f, 0.1f, 0.1f);
+        Debug.Log(ColorUtility.ToHtmlStringRGB(color));
+    }
+}
