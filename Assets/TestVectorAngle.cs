fb14efdbb2e54c89974238c2868564172312cdc3
Flag <karexy@qq.com>
2018-09-06 18:46:03 +0800

    update

---
+++ Assets/TestVectorAngle.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestVectorAngle : MonoBehaviour
+{
+    public Vector3 TestVec;
+
+    private void Update()
+    {
+        Debug.Log(Vector3.SignedAngle(Vector3.right, TestVec, Vector3.forward));
+    }
+}
