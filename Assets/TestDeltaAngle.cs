aa0f9b3a0ba709215b35e157beab2b3d4251dfd9
Flag <karexy@qq.com>
2018-11-09 20:08:02 +0800

    update

---
+++ Assets/TestDeltaAngle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDeltaAngle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // [-180, 180]
+        Debug.Log(Mathf.DeltaAngle(Camera.main.transform.eulerAngles.y, transform.eulerAngles.y));
+	}
+}
