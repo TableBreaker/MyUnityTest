ba73d5c89ba65522552f7dbd1cf4bf39deaaea56
luyuanpeng <karexy@qq.com>
2019-06-14 15:42:05 +0800

    push code

---
+++ Assets/TestPrefab.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPrefab : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var prefab = Resources.Load<GameObject>("TestPrefab");
+        Instantiate(prefab, transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
